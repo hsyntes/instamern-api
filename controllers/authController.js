e3ebbe1ade64e4eefd9032164fd4b3a98c3b7231
hsyntes <se.hsyntes@gmail.com>
2023-08-01 00:06:25 +0300

    configure token

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,9 +16,9 @@ const sendToken = (res, statusCode, user, message) => {
     expires: new Date(
       Date.now() + parseInt(process.env.JWT_EXPIRES_IN) * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
-    secure: false,
-    // sameSite: "lax",
+    // httpOnly: true,
+    // secure: false,
+    sameSite: "lax",
   });
 
   user.password = undefined;
