9983c0e0aca31d972050ddc6a2b594acec709e39
hsyntes <se.hsyntes@gmail.com>
2023-07-31 21:24:52 +0300

    deploying the api

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,8 +17,8 @@ const sendToken = (res, statusCode, user, message) => {
     expires: new Date(
       Date.now() + parseInt(process.env.JWT_EXPIRES_IN) * 24 * 60 * 60 * 1000
     ),
-    // httpOnly: true,
-    secure: false,
+    httpOnly: true,
+    secure: true,
     sameSite: "lax",
   });
 
