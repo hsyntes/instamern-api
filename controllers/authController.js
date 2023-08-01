8e3479d5d9095b80f9293c1da11558ea8fc5f6d0
hsyntes <se.hsyntes@gmail.com>
2023-08-01 05:34:14 +0300

    set cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,7 @@ const sendToken = (res, statusCode, user, message) => {
     expires: new Date(
       Date.now() + parseInt(process.env.JWT_EXPIRES_IN) * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
+    httpOnly: false,
     secure: process.env.NODE_ENV === "production",
     sameSite: "none",
   });
